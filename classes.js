2c9d2a65c31a9e11d3da33f2f10dfbeef460acae
L-Karimi <luciahkei101@gmail.com>
2022-07-11 18:42:46 +0300

    added prototype method in classes

--- classes.js
+++ classes.js
@@ -8,8 +8,12 @@ class students {
         return "I love praying"
 
       }
+      students.prototype.country=function(){
+        return "I love my country Kenya"
+      }
     }
   }
   var firstStudent=new students("Lucy",23,"AkiraChix");
   console.log(firstStudent.school)
   console.log(firstStudent.hobby())
+  console.log(firstStudent.country())
