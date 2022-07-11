4d096a8d0bcc43171f520543edee017a3aa6c7fd
L-Karimi <luciahkei101@gmail.com>
2022-07-11 18:22:07 +0300

    added objects

--- objects.js
+++ objects.js
@@ -14,3 +14,30 @@ function students(a,callback){
     console.log(callback)
 }
 students(26,people())
+
+console.log(a.splice(0,2))
+console.log(a)
+// let p=(
+//     age=22,
+//     country="Kenya",
+//     hobby (function(){
+//         console.log("My name is " + this.name + "and my hobby is is cooking")
+//         return "my favourite hobby is singing";
+//     })
+   
+// );
+console.log(p["age"]);
+console.log(p.country);
+var person=new Object(p);
+console.log(person.name);
+// person("age")=35;
+console.log(p);
+
+for(key of Object.entries(person)){
+    console.log(key.value)
+}
+// freezes a function and prevents modificatuon-like using const
+Object.freeze(person)
+p["school"] ="AkiraChix"
+console.log(person)
+ 
