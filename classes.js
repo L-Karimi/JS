8127585aa5d70f34b346758b32a1fa42e6f5b796
L-Karimi <luciahkei101@gmail.com>
2022-07-11 18:29:12 +0300

    added classes file

---
+++ classes.js
@@ -0,0 +1,11 @@
+// classes
+class students {
+    constructor(name, age, school) {
+      this.name = name;
+      this.age=age
+      this.school=school
+    }
+  }
+  var firstStudent=new students("Lucy",23,"AkiraChix");
+  console.log(firstStudents)
+  
--- objects.js
+++ objects.js
@@ -1,22 +1,22 @@
-let a=["bananas","mangoes","apples"]
-let c= new Array("Banana","Kiwi")
-console.log(a)
-console.log(c.push("tomatoes"))
-let b =c.unshift("tomatoes")
-console.log(b)
-let d= c.map(item=>"I love " +item)
-console.log(d)
-function people(){
-    console.log("You guys")
+let a = ["bananas", "mangoes", "apples"];
+let c = new Array("Banana", "Kiwi");
+console.log(a);
+console.log(c.push("tomatoes"));
+let b = c.unshift("tomatoes");
+console.log(b);
+let d = c.map((item) => "I love " + item);
+console.log(d);
+function people() {
+  console.log("You guys");
 }
-function students(a,callback){
-    console.log(a)
-    console.log(callback)
+function students(a, callback) {
+  console.log(a);
+  console.log(callback);
 }
-students(26,people())
+students(26, people());
 
-console.log(a.splice(0,2))
-console.log(a)
+console.log(a.splice(0, 2));
+console.log(a);
 // let p=(
 //     age=22,
 //     country="Kenya",
@@ -24,20 +24,20 @@ console.log(a)
 //         console.log("My name is " + this.name + "and my hobby is is cooking")
 //         return "my favourite hobby is singing";
 //     })
-   
+
 // );
 console.log(p["age"]);
 console.log(p.country);
-var person=new Object(p);
+var person = new Object(p);
 console.log(person.name);
 // person("age")=35;
 console.log(p);
 
-for(key of Object.entries(person)){
-    console.log(key.value)
+for (key of Object.entries(person)) {
+  console.log(key.value);
 }
 // freezes a function and prevents modificatuon-like using const
-Object.freeze(person)
-p["school"] ="AkiraChix"
-console.log(person)
- 
+Object.freeze(person);
+p["school"] = "AkiraChix";
+console.log(person);
+
